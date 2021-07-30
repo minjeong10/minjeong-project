eeea5a06a8ffd81ce0b023f6644b868f4d392997
DDJ-Seo <dongjoo0714@gmail.com>
2021-07-30 16:47:41 +0900

    section-4까지 작업, css 수정

--- docs/common.js
+++ docs/common.js
@@ -1,49 +1,49 @@
 gsap.from('.img-1', {
   ease: Expo.easeIn,
   opacity:0,
-  duration:1,
+  duration:0.7,
   scrollTrigger:{
     trigger:'.img-1',
-    start:'0 70%'
+    start:'0 75%'
   }
 });
 
 gsap.from('.img-2', {
   ease: Expo.easeIn,
   opacity:0,
-  duration:1,
+  duration:0.7,
   scrollTrigger:{
     trigger:'.img-2',
-    start:'0 80%'
+    start:'0 85%'
   }
 });
 
 gsap.from('.img-3', {
   ease: Expo.easeIn,
   opacity:0,
-  duration:1,
+  duration:0.7,
   scrollTrigger:{
     trigger:'.img-3',
-    start:'0 80%'
+    start:'0 85%'
   }
 });
 
 gsap.from('.img-4', {
   ease: Expo.easeIn,
   opacity:0,
-  duration:1,
+  duration:0.7,
   scrollTrigger:{
     trigger:'.img-4',
-    start:'0 80%'
+    start:'0 85%'
   }
 });
 
 gsap.from('.img-5', {
   ease: Expo.easeIn,
   opacity:0,
-  duration:1,
+  duration:0.7,
   scrollTrigger:{
     trigger:'.img-5',
-    start:'0 80%'
+    start:'0 85%'
   }
 });
