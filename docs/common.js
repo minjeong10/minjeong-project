19de248bccefc79322524e562b5d204c40504c38
DDJ-Seo <dongjoo0714@gmail.com>
2021-07-30 16:45:12 +0900

    section-4 까지 작업

--- docs/common.js
+++ docs/common.js
@@ -4,6 +4,46 @@ gsap.from('.img-1', {
   duration:1,
   scrollTrigger:{
     trigger:'.img-1',
-    start:'0 90%'
+    start:'0 70%'
+  }
+});
+
+gsap.from('.img-2', {
+  ease: Expo.easeIn,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.img-2',
+    start:'0 80%'
+  }
+});
+
+gsap.from('.img-3', {
+  ease: Expo.easeIn,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.img-3',
+    start:'0 80%'
+  }
+});
+
+gsap.from('.img-4', {
+  ease: Expo.easeIn,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.img-4',
+    start:'0 80%'
+  }
+});
+
+gsap.from('.img-5', {
+  ease: Expo.easeIn,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.img-5',
+    start:'0 80%'
   }
 });
